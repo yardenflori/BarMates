26a94fa6903a85828fc5be7522280b564f7f0b35
Shaked <shakedgana1@gmail.com>
2019-11-25 13:55:08 +0200

    Test Shaked

--- BarMates/App_Code/Bar.cs
+++ BarMates/App_Code/Bar.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-
+//Test shaked
 public class Bar
 {
     //Add here all fields from DB with the same pattern
     public int barId { get; set; }
     public string barName { get; set; }
-
+    
     public Bar()
 	{
 
