5db2840bfe3cac60767172e2ed85d6a4a9cdb8b7
Shaked <shakedgana1@gmail.com>
2019-12-07 13:25:40 +0200

    Collaborative filtering folder

---
+++ BarMates/App_Code/CollaborativeFiltering/BarsTagsMatrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BarsTagsMatrix
+/// </summary>
+public class BarsTagsMatrix
+{
+    public BarsTagsMatrix()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
