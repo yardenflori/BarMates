ce4697896c9b00a6f76760c70da94d9a10897132
Yuval <yuvalc1195@gmail.com>
2019-12-07 16:47:37 +0200

    added bartagmatrix method

--- BarMates/App_Code/CollaborativeFiltering/BarsTagsMatrix.cs
+++ BarMates/App_Code/CollaborativeFiltering/BarsTagsMatrix.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-/// <summary>
-/// Summary description for BarsTagsMatrix
-/// </summary>
 public class BarsTagsMatrix
 {
+    public int[][] GetBarTagsMatrix(List<Bar> bars)
+    {
+        int n = bars.Count;
+        int[][] barsMatrix = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            barsMatrix[i] = new int[bars[i].BarCharacteristics.Length + 1];
+            barsMatrix[i][0] = bars[i].BarId;
+            for (int j = 1; j < bars[i].BarCharacteristics.Length + 1; j++)
+            {
+                barsMatrix[i][j] = bars[i].BarCharacteristics[j];
+            }
+        }
+        return barsMatrix;
+    }
     public BarsTagsMatrix()
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        
     }
 }
