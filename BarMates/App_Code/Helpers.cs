846ad3f47bb1ca7782cf1f2bb1c21f338d9593d5
Yuval <yuvalc1195@gmail.com>
2019-12-07 13:16:47 +0200

    bug fixed

--- BarMates/App_Code/Helpers.cs
+++ BarMates/App_Code/Helpers.cs
@@ -1,16 +1,16 @@
-using System.Math;
+using System;
 
 public static class Helpers
 {
     /*the vectors are interest vectors, it have to be the same length*/
     public static double InterestsVectorDistance(double[] vector1, double[] vector2)
     {
-        int sum = 0;
+        double sum = 0;
         for (int i = 0; i < vector1.Length; i++)
         {
             sum += Math.Pow((vector1[i] - vector2[i]), 2.0);
         }
-        sum = Math.sqrt(sum);
+        sum = Math.Sqrt(sum);
         return sum;
     }
 }
