61d4eb87e4700f81f8c074309f89520e11b2911b
Shaked <shakedgana1@gmail.com>
2019-12-07 14:29:46 +0200

    Add bar characteristic vector

--- BarMates/App_Code/CollaborativeFiltering/UserTagsMatrix.cs
+++ BarMates/App_Code/CollaborativeFiltering/UserTagsMatrix.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-/// <summary>
-/// Summary description for UserTagsMatrix
-/// </summary>
+﻿
 public class UserTagsMatrix
 {
     public UserTagsMatrix()
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        
     }
 }
