3a0fd17f23efaf1bd49f97cad049333fa4f903bf
yarden flori <yarden.flori@gmail.com>
2020-01-18 00:38:56 +0200

    chal

--- BarMates/challenges.aspx.cs
+++ BarMates/challenges.aspx.cs
@@ -1,8 +1,13 @@
 ﻿using BarMates;
+using Newtonsoft.Json;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -14,5 +19,24 @@ public partial class challenges : System.Web.UI.Page
         {
             Response.Redirect("Default.aspx");
         }
+    }
+
+    [WebMethod]
+    public static string RatedBars()
+    {
+        ArrayList rates;
+        List<string> userRates = new List<string>();
+        string userName = DBController.GetUserName();
+        List<SqlParameter> parameters = new List<SqlParameter>();
+        parameters.Add(new SqlParameter("userName", userName));
+        rates = DBController.ExecuteStoredProcedure_Select("sp_get_all_rating_by_userName", parameters);
+        if (rates.Count > 0)
+        {
+            foreach (DbDataRecord currentItem in rates)
+            {
+                userRates.Add(currentItem["barGoogleId"].ToString());
+            }
+        }
+        return JsonConvert.SerializeObject(userRates);
     }
 }
